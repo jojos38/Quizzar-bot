5c800947472dbb605bebfee1c5abf210279b9ba9
jojos38 <jojos38000@hotmail.com>
2021-04-08 16:39:36 +0200

    Removed useless files / Fixes default questions amount
    Removed useless files / Fixes default questions amount

--- gamemodes/classic-game.js
+++ gamemodes/classic-game.js
@@ -220,17 +220,16 @@ class ClassicGame extends Game {
 			difficulty = await this.#db.getSetting(guildID, "defaultDifficulty");
 
 		// If / Not below 1 / Not above 100 / Is an int and is not null / Is not equal to 0
-		if (((questionsAmount < 1 || questionsAmount > 100 || !tools.isInt(questionsAmount)) && questionsAmount != 0)) {
+		if (((questionsAmount < 1 || questionsAmount > 100 || !tools.isInt(questionsAmount)) && questionsAmount)) {
 			tools.sendCatch(this._channel, lm.getEb(this._lang).getBadQuesEmbed());
             this._terminate();
             return;
 		}
 		else {
-			if (questionsAmount == 0)
-				if (this._guild.member(this._userID).hasPermission("MANAGE_MESSAGES"))
-					questionsAmount = 2147483647;
-				else
-					questionsAmount = await this.#db.getSetting(guildID, "defaultQuestionsAmount");
+			if (this._guild.member(this._userID).hasPermission("MANAGE_MESSAGES"))
+				questionsAmount = 2147483647;
+			else
+				questionsAmount = await this.#db.getSetting(guildID, "defaultQuestionsAmount");
 		}
 
 		this.#difficulty = Number(difficulty);
